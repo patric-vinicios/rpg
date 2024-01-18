1cc361e3ce02a0880e92f8d413471bba393bba6b
Patric Vinicios <psdev7@gmail.com>
2024-01-18 20:17:09 -0300

    feat: add hash password

--- lib/api/users/user.ex
+++ lib/api/users/user.ex
@@ -8,6 +8,8 @@ defmodule Api.Users.User do
   import Ecto.SoftDelete.Schema
 
   alias Api.Helpers.Validators
+  alias Api.Providers.Hash.Client, as: ArgonClient
+  alias Ecto.Changeset
   alias Ecto.Enum
 
   @type t :: %__MODULE__{
@@ -21,13 +23,14 @@ defmodule Api.Users.User do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
 
-  @fields [:username, :email, :password_hash, :role]
-  @required_fields [:username, :email, :password_hash]
+  @fields [:username, :email, :password, :role]
+  @required_fields [:username, :email, :password]
 
   schema "users" do
     field :username, :string
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
     field :role, Enum, values: [:admin, :mod, :player]
 
     timestamps()
@@ -39,6 +42,14 @@ defmodule Api.Users.User do
     |> cast(params, @fields)
     |> validate_required(@required_fields)
     |> validate_format(:email, Validators.email())
+    |> validate_length(:password, min: 8)
+    |> hash_password()
     |> unique_constraint(:email)
   end
+
+  defp hash_password(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password_hash: ArgonClient.hash_password(password))
+  end
+
+  defp hash_password(changeset), do: changeset
 end
